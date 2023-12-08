c22483c30eaf522306a7d7fff1c90c4490f3cc79
devivanova <119169640+devivanova@users.noreply.github.com>
2023-12-08 12:37:32 +0100

    added tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: Person[] = [];
+
+  protected constructor(
+    protected door: boolean,
+    protected key: Key,
+  ) {}
+
+  abstract openDoor(key: Key): void;
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("The door is closed. Person cannot enter.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
